55da9e82f84300eb67877f31cbe0ba7536709317
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-11-18 01:01:59 -0800

    Create honey.sh

---
+++ honey.sh
@@ -0,0 +1,3 @@
+docker run honeygain/honeygain -tou-get
+docker run --restart unless-stopped honeygain/honeygain -tou-accept -email akeelaasath20@gmail.com -pass 20000317 -device c2ptech
+docker run --restart unless-stopped honeygain/honeygain -tou-accept -email akeelaasath20@gmail.com -pass 20000317 -device c2ptech1
