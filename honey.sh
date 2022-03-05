010dccfbc6092eeffd17a0e7b30a4e0e57ad25b2
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2022-03-05 20:11:18 -0300

    Update honey.sh

--- honey.sh
+++ honey.sh
@@ -1,2 +1,2 @@
-docker run --restart unless-stopped honeygain/honeygain -tou-accept -email akeelaasath20@gmail.com -pass 20000317 -device c2ptech
-docker run --restart unless-stopped honeygain/honeygain -tou-accept -email akeelaasath20@gmail.com -pass 20000317 -device c2ptech1
+docker run --restart unless-stopped honeygain/honeygain -tou-accept -email luanwillianzh@gmail.com -pass lw967504 -device c2ptech
+docker run --restart unless-stopped honeygain/honeygain -tou-accept -email luanwillianzh@gmail.com -pass lw967504 -device c2ptech1
